ecc382ead0aad9a3996013c74566a3499848301d
takumi-pro <takumidiary.0927@gmail.com>
2022-01-20 01:15:52 +0900

    rap

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 const startBtn = document.querySelector('.start') as HTMLButtonElement
 const stopBtn = document.querySelector('.stop') as HTMLButtonElement
 const resetBtn = document.querySelector('.reset') as HTMLButtonElement
+const rapBtn = document.querySelector('.rap') as HTMLButtonElement
+const rapTimeElementWrap = document.querySelector('.rap_time') as HTMLDivElement
 
 const hourDisplay = document.querySelector('.hour') as HTMLSpanElement
 const minuteDisplay = document.querySelector('.minute') as HTMLSpanElement
@@ -12,9 +14,16 @@ const MSEC_MINUTE = MSEC_SEC * 60
 const MSEC_HOUR = MSEC_MINUTE * 60
 
 let startTime: number
+let rapStartTime: number
 let timeoutId: number
+let previousRapTime: number
 let timeToStop = 0
 let isStart = false
+let isRap = false
+
+if (!isStart) {
+    rapBtn.disabled = true
+}
 
 function countUp() {
     timeoutId = window.setTimeout(() => {
@@ -52,11 +61,13 @@ function start() {
     if (isStart) return
 
     isStart = true
+
     startTime = Date.now()
     countUp()
 
     startBtn.style.display = 'none'
     stopBtn.style.display = 'block'
+    rapBtn.disabled = false
     resetBtn.disabled = true
 }
 
@@ -64,15 +75,37 @@ function stop() {
     if (!isStart) return
 
     isStart = false
+
     clearTimeout(timeoutId)
     timeToStop += Date.now() - startTime
 
     startBtn.style.display = 'block'
     stopBtn.style.display = 'none'
+    rapBtn.disabled = true
     resetBtn.disabled = false
+}
+
+function rap() {
+    rapStartTime = Date.now()
+
+    if (!isRap) {
+        let para = document.createElement('p')
+        let content = document.createTextNode(String(rapStartTime - startTime))
+        para.appendChild(content)
+        rapTimeElementWrap.appendChild(para)
+    } else {
+        let para = document.createElement('p')
+        let content = document.createTextNode(String(rapStartTime - previousRapTime))
+        para.appendChild(content)
+        rapTimeElementWrap.appendChild(para)
+    }
+
+    isRap = true
+    previousRapTime = rapStartTime
 }
 
 startBtn.addEventListener('click', start)
 stopBtn.addEventListener('click', stop)
+rapBtn.addEventListener('click', rap)
 resetBtn.addEventListener('click', reset)
 
