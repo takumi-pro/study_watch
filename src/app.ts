ff832927d03a96a6d56b4d776f0315f4c7d46c0f
takumi-pro <takumidiary.0927@gmail.com>
2022-01-22 12:26:02 +0900

    秒数カウントでグラフ増加

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
-const Highcharts = require('highcharts')
-require('highcharts/modules/exporting')(Highcharts)
-require('highcharts/modules/data.js')(Highcharts)
+import Highcharts from 'highcharts'
+import ex from 'highcharts/modules/exporting'
+import data from 'highcharts/modules/data.js'
+
+ex(Highcharts)
+data(Highcharts)
 
 // html要素の取得
 const startBtn = document.querySelector('.start') as HTMLButtonElement
@@ -12,55 +15,60 @@ const inputArea = document.querySelector('.subject_name') as HTMLInputElement
 const chartArea = document.querySelector('.chart_area') as HTMLDivElement
 const timer = document.querySelector('.timer') as HTMLDivElement
 
-let isStart: boolean = false
+let isStart = false
 let startTime: number
 let intervalId: number
-let timeToStop: number = 0
+let graphIntervalId: number
+let timeToStop = 0
+let count = 0
 let chart: any
 
-buttonDisabled(true)
-
-chart = new Highcharts.chart({
+// グラフ描画
+// 空のデータを入れる
+chart = new (Highcharts as any).chart({
     chart: {
-        renderTo: "chart_area",
-        type: "column",
+        renderTo: 'chart_area',
+        type: 'column',
     },
     xAxis: {
-        categories: [1,2,3,4,5]
+        categories: ['ネットワーク'],
     },
     yAxis: {
+        softMax: 110,
         title: {
-            text: 'number'
-        }
+            text: 'number',
+        },
     },
-    series: [
-        { type:'column', data: [5, 4, 1, 2, 10] },
-    ],
-});
+    series: [{ type: 'column', name: '科目1', data: [0] }],
+})
+
+//全ボタンを無効化
+// buttonDisabled(true)
 
 function countUp(difTime: number) {
-    let hour = difTime / 3600000
-    let min = difTime / 60000
-    let sec = difTime % 60000 / 1000
-    let msec = difTime % 1000
+    const hour = difTime / 3600000
+    const min = difTime / 60000
+    const sec = (difTime % 60000) / 1000
+    const msec = difTime % 1000
 
-    let hh = Math.floor(hour)
-    let mm = Math.floor(min)
-    let ss = Math.floor(sec)
-    let ms = Math.floor(msec)
+    const hh = Math.floor(hour)
+    const mm = Math.floor(min)
+    const ss = Math.floor(sec)
+    const ms = Math.floor(msec)
 
-    let formatedHH = hh.toString().padStart(2, '0')
-    let formatedMM = mm.toString().padStart(2, '0')
-    let formatedSS = ss.toString().padStart(2, '0')
-    let formatedMS = ms.toString().padStart(3, '0')
+    //時間、分、秒は2桁でm秒は3桁に修正
+    const formatedHH = hh.toString().padStart(2, '0')
+    const formatedMM = mm.toString().padStart(2, '0')
+    const formatedSS = ss.toString().padStart(2, '0')
+    const formatedMS = ms.toString().padStart(3, '0')
 
     return `${formatedHH}:${formatedMM}:${formatedSS}:${formatedMS}`
 }
 
 function showButton(showBtn: string) {
-    let buttonToShow = (showBtn === 'START') ? startBtn : stopBtn
-    let buttonToHide = (showBtn === 'START') ? stopBtn : startBtn
-    isStart = (showBtn === 'STOP') ? true : false
+    const buttonToShow = showBtn === 'START' ? startBtn : stopBtn
+    const buttonToHide = showBtn === 'START' ? stopBtn : startBtn
+    isStart = showBtn === 'STOP' ? true : false
     buttonToShow.style.display = 'block'
     buttonToHide.style.display = 'none'
 }
@@ -69,30 +77,54 @@ function print(txt: string) {
     timer.innerHTML = txt
 }
 
+function drawInit() {
+    count = 0
+    chart.series[0].update({ data: [0] }, false)
+    chart.redraw()
+}
+
+function graphUpdate(difTime: number) {
+    const sec = Math.floor(difTime / 1000)
+
+    chart.series[0].update(
+        {
+            data: [sec * 5],
+        },
+        false
+    )
+    chart.redraw()
+}
+
 function start() {
     showButton('STOP')
-    // drawChart()
 
     inputArea.disabled = true
 
     startTime = Date.now()
     intervalId = window.setInterval(() => {
-        let difTime = Date.now() - startTime + timeToStop
+        const difTime = Date.now() - startTime + timeToStop
         print(countUp(difTime))
     }, 10)
+
+    graphIntervalId = window.setInterval(() => {
+        const difTime = Date.now() - startTime + timeToStop
+        graphUpdate(difTime)
+    }, 10000)
 }
 
 function stop() {
     showButton('START')
     timeToStop += Date.now() - startTime
     clearInterval(intervalId)
+    clearInterval(graphIntervalId)
 }
 
 function reset() {
     if (!isStart) {
         inputArea.disabled = false
         inputArea.value = ''
-        buttonDisabled(true)
+        buttonDisabled(false)
+        drawInit()
     }
     timeToStop = 0
     print('00:00:00:000')
@@ -107,48 +139,13 @@ function buttonDisabled(isDisabled: boolean) {
 
 function input(e: Event) {
     if (!(e.target instanceof HTMLInputElement)) return
-    
+
     if (e.target.value) {
         buttonDisabled(false)
     } else {
         buttonDisabled(true)
     }
 }
-
-function drawChart() {
-    chart = new Highcharts.chart({
-        chart: {
-            renderTo: "chart_area",
-            type: "column",
-            event: {
-                load: function (series: any) {
-                    series.data
-                }
-            }
-        },
-        series: [
-            { type:'column', data: [5, 4, 1, 2] },
-        ],
-    });
-}
-const btn = document.querySelector('.btn') as HTMLButtonElement
-btn.addEventListener('click', () => {
-    console.log('click')
-    let random1 = Math.floor(Math.random() * 100 + 1)
-    let random2 = Math.floor(Math.random() * 100 + 1)
-    let random3 = Math.floor(Math.random() * 100 + 1)
-    let random4 = Math.floor(Math.random() * 100 + 1)
-    chart.series[0].update({
-        data: [random1, random2, random3, random4]
-    }, false);
-    chart.addSeries({
-        data: [5, 4, 1, 2, 10]
-    })
-    chart.redraw()
-})
-
-
-
 
 inputArea.addEventListener('input', input)
 startBtn.addEventListener('click', start)
